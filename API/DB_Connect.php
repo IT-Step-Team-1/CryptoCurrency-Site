0d89abae34d38e921a39b18424f349bdd995d294
DJmoster <Zapotichnyi12@gmail.com>
2020-06-13 14:20:31 +0300

    Добавив скріпт до таймера

--- API/DB_Connect.php
+++ API/DB_Connect.php
@@ -2,6 +2,10 @@
 
 require "Config.php";
 
+if ($_GET["pass"] != $passphrase) {
+  die("ERROR PASSPHRASE");
+}
+
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -9,8 +13,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO Transactions (ETH_Adress, Value, Status)
-VALUES ('0xdaskjadsdasjasdad', '100', 'False')";
+$sql = "INSERT INTO Transactions (ETH_Address, Value, Status) VALUES ('0xdaskjadsdasjasdad', '100', 'False')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
--- API/DB_Get.php
+++ API/DB_Get.php
@@ -1,38 +1,49 @@
 <?php
 
+header('HTTP/1.1 200 OK');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
 require "Config.php";
+
+$ERROR["status"] 	= "ERROR";
+$DONE["status"]		= "DONE";
+$NONE["status"]   = "NONE";
 
 
 if ($_GET["pass"] != $passphrase) {
-    die("ERROR PASSPHRASE");
+    die(json_encode($ERROR, JSON_UNESCAPED_UNICODE));
 }
+
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  die(json_encode($ERROR, JSON_UNESCAPED_UNICODE));
 }
 
 
-$sql = "SELECT ID, ETH_Adress, Value, Status FROM Transactions";
+$sql = "SELECT ID, ETH_Address, Value, Status FROM Transactions";
 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    $data = NULL;
+	
+    $data["status"] = "DONE";
 
     while($row = $result->fetch_assoc()) {
-        $data[$row["ID"]] = [   "ETH_Adress"    => $row["ETH_Adress"], 
-                                "Value"         => $row["Value"], 
-                                "Status"        => $row["Status"]
-                            ];
+        $data["data"][$row["ID"]] = [   "ETH_Address"    => $row["ETH_Address"], 
+                                        "Value"         => $row["Value"], 
+                                        "Status"        => $row["Status"],
+                                    ];
     }
     
-    echo("<pre style='word-wrap: break-word; white-space: pre-wrap;'>". json_encode($data). "</pre>");
+    echo(json_encode($data, JSON_UNESCAPED_UNICODE));
 
   } 
   else {
-    echo("0 results");
+	  
+    echo(json_encode($NONE, JSON_UNESCAPED_UNICODE));
   }
   
 
